e43cf83e014085cb1b9ab6191c20285a5e99f180
kayi <747735562@qq.com>
2022-10-19 22:40:58 +0800

    18～19视频内容修改

---
+++ pages/tabs demo/tabs demo.vue
@@ -0,0 +1,102 @@
+<template>
+	<view>
+		<!-- 基本菜单切换 -->
+		<view class="flex">
+			<view :class="['box',{'dark':isActive}]" @tap="handleTap(true)">
+				菜单一
+			</view>
+			<view :class="['box',{'dark':!isActive}]" @tap="handleTap(false)">
+				菜单二
+			</view>
+		</view>
+		<view class="box cont margin" v-if="isActive">
+			内容一
+		</view>
+		<view class="box cont margin" v-else="!isActive">
+			内容二
+		</view>
+		<!-- 拓展菜单切换 -->
+		<view class="flex">
+			<view v-for="(item,index) in tabsArr" 
+			:class="['box',{'dark':index==tabIdx}]"
+			@tap="tabIdx=index">
+				{{item}}
+			</view>
+		</view>
+		<view v-for="(item,index) in tabsArr" class="box cont margin" v-if="index==tabIdx">
+			这是{{item}}的内容
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isActive:false,
+				tabsArr:[
+					'首页','新闻','产品','联系我们','招聘'
+				],
+				tabIdx:0
+			}
+		},
+		methods: {
+			handleTap(bool){
+				this.isActive=bool
+				
+			}
+		}
+	}
+</script>
+
+<style>
+.flex{
+	display: flex;
+	justify-content: center;
+}
+.margin {
+		margin: 10upx;
+	}
+
+	.box {
+		height: 100upx;
+		background-color: #65728c;
+		text-align: center;
+		color: #1a0032;
+		font-size: 30upx;
+		line-height: 100upx;
+		padding: 0 30upx;
+	}
+
+	.box.active {
+		background-color: #1900f5;
+	}
+
+	.box1 {
+		height: 200upx;
+		background-color: #708e00;
+	}
+	.dark
+	{
+		color: #e9eadb;
+		text-align: center;
+		background-color: #0f0118;
+	}
+	.box.dark{
+		height: 100upx;
+		line-height: 100upx;
+	}
+	.margin-left{
+		color: #e9e9e9;
+		text-align: center;
+	}
+	.cont{
+		color: #344209;
+		height: 200upx;
+		font-weight: 200upx;
+		line-height: 200upx;
+		background-color: #d6c4ed;
+		font-size: 30upx;
+		display: flex;
+	}
+</style>
---
+++ pages/v-for-nest/v-for-nest.vue
@@ -0,0 +1,83 @@
+<template>
+	<view>
+		<view v-for="(item,index) in wulin ">
+			<view class="box dark margin" @tap="skillIdx=index">
+				门派：{{item.title}} 代表：{{item.represent}}
+			</view>
+			<view v-if="skillIdx==index">
+				<view v-for="(itm,idx) in item.skill" class="box margin margin-left">
+					{{itm}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				wulin:[
+					{
+						title:'少林',
+						represent:'扫地僧',
+						skill:[
+							'三十六路拳脚',
+							'七十二绝技',
+							'易筋经',
+							'金钟罩',
+							'铁布衫'
+						]
+					},
+					{
+						title:'武当',
+						represent:'张三丰',
+						skill:[
+							'梯云纵',
+							'太极拳法',
+							'纯阳无极功'
+						],
+						
+					}
+				],
+				skillIdx:-1
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.margin {
+		margin: 10upx;
+	}
+
+	.box {
+		height: 100upx;
+		background-color: #65728c;
+		text-align: center;
+		color: #4f37f4;
+		font-size: 30upx;
+		line-height: 100upx;
+	}
+
+	.box.active {
+		background-color: #1900f5;
+	}
+
+	.box1 {
+		height: 200upx;
+		background-color: #708e00;
+	}
+	.dark
+	{
+		color: #e9eadb;
+		text-align: center;
+	}
+	.margin-left{
+		color: #e9e9e9;
+		text-align: center;
+	}
+</style>
