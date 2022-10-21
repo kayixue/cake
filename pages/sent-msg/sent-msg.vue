a8038eeefa6dabdebbd6ea0e197971ef15b0e07e
kayi <747735562@qq.com>
2022-10-22 00:42:57 +0800

    小修改

---
+++ pages/sent-msg/sent-msg.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<view class="flex margin">
+			<input type="text" value="">
+			<button type="primary">发送</button>
+		</view>
+		<!-- <view class="box"></view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+input{
+	height: 100upx;
+	border: 1upx solid #9BA7Bf;
+	flex: 1;
+}
+.flex{
+	display: flex;
+	justify-content: center;
+}
+.margin{
+	margin: 10upx;
+}
+.box{
+	background-color: #9BA7Bf;
+	text-align: center;
+	padding:0 30upx
+}
+</style>
